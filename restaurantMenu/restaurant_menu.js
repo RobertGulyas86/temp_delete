0dc46ccee8e45ad4dc5ee073fd3c6e847da3c185
Your Name <you@example.com>
2025-10-03 09:54:33 -0400

    message

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,4 +1,4 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
 const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
 const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
@@ -16,3 +16,8 @@ for (let i = 0; i < dessertMenu.length; i++) {
     dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;
 }
 document.getElementById('dessertMenuItems').innerHTML = dessertItem;
+
+document.getElementById('breakfastTotalItems').innerHTML = `Total menu items: ${breakfastMenu.length}`;
+document.getElementById('maincourseTotalItems').innerHTML = `Total menu items: ${mainCourseMenu.length}`;
+document.getElementById('dessertTotalItems').innerHTML = `Total menu items: ${dessertMenu.length}`;
+
